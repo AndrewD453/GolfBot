c629c1f47ba83efaaf2ab3f17597b1d3c1470668
Andrew Dailey <ajdailey5@gmail.com>
2022-03-22 22:02:07 -0400

    Major updates to transmit and recieve
    "Final" versions

--- Golf_Mainboard/Golf_Mainboard.ino
+++ Golf_Mainboard/Golf_Mainboard.ino
@@ -1,114 +1,165 @@
 #include <SPI.h>
 #include <nRF24L01.h>
 #include <RF24.h>
-RF24 radio(8, 10); // CE, CSN
 
-#define iters 2
+RF24 radio(48, 53); // CE, CSN
 
-const int trigPinL = A5;
-const int echoPinL = A4;
-const int trigPinR = A1;
-const int echoPinR = A0;
-const int m1A = 4;
-const int m1B = 5;
-const int m1Spd = 6;
-const int m2A = 7;
-const int m2B = 2;
-const int m2Spd = 3;
+
+#include <NMEAGPS.h>
+#include <GPSport.h>
+#include <Adafruit_BNO08x.h>
+#define BNO08X_RESET -1
+#define NEOGPS_USE_SERIAL1
+
+NMEAGPS gps;
+gps_fix fix;
+Adafruit_BNO08x  bno08x(BNO08X_RESET);
+sh2_SensorValue_t sensorValue;
+
+struct euler_t {
+  float yaw;
+  float pitch;
+  float roll;
+} ypr;
+
+#ifdef FAST_MODE
+  // Top frequency is reported to be 1000Hz (but freq is somewhat variable)
+  sh2_SensorId_t reportType = SH2_GYRO_INTEGRATED_RV;
+  long reportIntervalUs = 2000;
+#else
+  // Top frequency is about 250Hz but this report is more accurate
+  sh2_SensorId_t reportType = SH2_ARVR_STABILIZED_RV;
+  long reportIntervalUs = 5000;
+#endif
+void setReports(sh2_SensorId_t reportType, long report_interval) {
+  Serial.println("Setting desired reports");
+  if (! bno08x.enableReport(reportType, report_interval)) {
+    Serial.println("Could not enable stabilized remote vector");
+  }
+}
+
+float distance;
+float myAngle;
+float angle;
+
+
+#define USIters 2
+
+const int trigPinL = 35;
+const int echoPinL = 34;
+const int trigPinR = 32;
+const int echoPinR = 33;
+const int motorL = 7;
+const int motorR = 6;
+const uint64_t pipe = 0xE6E6E6E6E6E6;
+const byte address[6] = "00001";
+
+char c, mode;
+float x, y;
+float v, w;
 int waited;
 long temp;
 long durationL, durationR;
 long distanceL, distanceR;
-bool buttonState = 1;
-const byte address[6] = "00001";
-bool written = false;
+bool ping = true;
+
 void setup() {
   pinMode(trigPinL, OUTPUT);
   pinMode(echoPinL, INPUT);
   pinMode(trigPinR, OUTPUT);
   pinMode(echoPinR, INPUT);
-  pinMode(m1A, OUTPUT);
-  pinMode(m1B, OUTPUT);
-  pinMode(m2A, OUTPUT);
-  pinMode(m2B, OUTPUT);
-  pinMode(m1Spd, OUTPUT);
-  pinMode(m2Spd, OUTPUT);
+  pinMode(motorL, OUTPUT);
+  pinMode(motorR, OUTPUT);
   Serial.begin(9600);
+  gpsPort.begin(9600);
+  if (!bno08x.begin_I2C()) {
+    Serial.println("Failed to find BNO08x chip");
+    while (1) { delay(10); }
+  }
+  setReports(reportType, reportIntervalUs);
   radio.begin();
-  radio.openWritingPipe(address);
+  radio.openReadingPipe(1, pipe);
   radio.setPALevel(RF24_PA_MAX);
-  radio.stopListening();
+  radio.startListening();
 }
 
 long msToIn(long ms) {
   return ms / 74/* / 2*/;
 }
 
-void write1(double mSpeed) { // right
-  Serial.print("L: ");
-  Serial.println(mSpeed * 255);
-  if (mSpeed > 1) {
-    mSpeed = 1;
-  }
-  if (mSpeed < -1) {
-    mSpeed = -1;
-  }
-  if (mSpeed == 0) {
-    digitalWrite(m1A, HIGH);
-    digitalWrite(m1B, HIGH);
-  }
-  else if (mSpeed > 0) {
-    digitalWrite(m1A, HIGH);
-    digitalWrite(m1B, LOW);
-    analogWrite(m1Spd, mSpeed * 255);
-  }
-  else {
-    digitalWrite(m1A, LOW);
-    digitalWrite(m1B, HIGH);
-    analogWrite(m1Spd, -mSpeed * 255);
+double getAngle(double x, double y) {
+  return (map(atan2(y, x) * 180 / PI, 180, -180, 0, 360) + 90) % 360;
+}
+
+void quaternionToEuler(float qr, float qi, float qj, float qk, euler_t* ypr, bool degrees = false) {
+
+    float sqr = sq(qr);
+    float sqi = sq(qi);
+    float sqj = sq(qj);
+    float sqk = sq(qk);
+
+    ypr->yaw = atan2(2.0 * (qi * qj + qk * qr), (sqi - sqj - sqk + sqr));
+    ypr->pitch = asin(-2.0 * (qi * qk - qj * qr) / (sqi + sqj + sqk + sqr));
+    ypr->roll = atan2(2.0 * (qj * qk + qi * qr), (-sqi - sqj + sqk + sqr));
+
+    if (degrees) {
+      ypr->yaw *= RAD_TO_DEG;
+      ypr->pitch *= RAD_TO_DEG;
+      ypr->roll *= RAD_TO_DEG;
+    }
+}
+
+void quaternionToEulerRV(sh2_RotationVectorWAcc_t* rotational_vector, euler_t* ypr, bool degrees = false) {
+    quaternionToEuler(rotational_vector->real, rotational_vector->i, rotational_vector->j, rotational_vector->k, ypr, degrees);
+}
+
+void quaternionToEulerGI(sh2_GyroIntegratedRV_t* rotational_vector, euler_t* ypr, bool degrees = false) {
+    quaternionToEuler(rotational_vector->real, rotational_vector->i, rotational_vector->j, rotational_vector->k, ypr, degrees);
+}
+
+void writeL(double mSpeed) {
+  Serial.print(mSpeed);
+  Serial.print(", ");
+  if (mSpeed >= 0) {
+    analogWrite(motorL, mSpeed * 255);
   }
 }
 
-void write2(double mSpeed) { // left
-  Serial.print("R: ");
-  Serial.println(mSpeed * 255);
-  if (mSpeed > 1) {
-    mSpeed = 1;
-  }
-  if (mSpeed < -1) {
-    mSpeed = -1;
-  }
-  if (mSpeed == 0) {
-    digitalWrite(m2A, HIGH);
-    digitalWrite(m2B, HIGH);
-  }
-  else if (mSpeed > 0) {
-    digitalWrite(m2A, HIGH);
-    digitalWrite(m2B, LOW);
-    analogWrite(m2Spd, mSpeed * 255);
-  }
-  else {
-    digitalWrite(m2A, LOW);
-    digitalWrite(m2B, HIGH);
-    analogWrite(m2Spd, -mSpeed * 255);
+void writeR(double mSpeed) {
+  Serial.print(mSpeed);
+  Serial.println();
+  if (mSpeed >= 0) {
+    analogWrite(motorR, mSpeed * 255);
   }
 }
 
-void loop() {
+void navManual() {
+  x = map(x, 0, 1023, 100, -100);
+  y = map(y, 0, 1023, -100, 100);
+  v = (100 - abs(x)) * (y / 100) + y;
+  w = (100 - abs(y)) * (x / 100) + x;
+  writeL((v - w) / 200.0); //LEFT ---- Algorithm: https://home.kendra.com/mauser/Joystick.html
+  writeR((v + w) / 200.0); //RIGHT
+}
+
+bool navUS() {
   durationL = 0;
   waited = 0;
-  for (int i = 0; i < iters; i++) {
-    radio.write(&buttonState, sizeof(buttonState));
+  radio.openWritingPipe(pipe);
+  radio.stopListening();
+  ping = true;
+  for (int i = 0; i < USIters; i++) {
+    radio.write(&ping, sizeof(ping));
     digitalWrite(trigPinL, HIGH);
     delayMicroseconds(10);
     digitalWrite(trigPinL, LOW);
     temp = pulseIn(echoPinL, HIGH);
-    if (temp > 30000 && waited < iters * 5) {
+    if (temp > 30000 && waited < USIters * 5) {
       i--;
       waited++;
     }
     else if (temp > 30000) {
-      durationL = iters * 40000;
+      durationL = USIters * 40000;
       break;
     }
     else {
@@ -116,7 +167,7 @@ void loop() {
     }
     delay(57);
   }
-  durationL /= iters;
+  durationL /= USIters;
   distanceL = msToIn(durationL);
   Serial.print(durationL);
   Serial.print(" L ");
@@ -124,18 +175,18 @@ void loop() {
 
   durationR = 0;
   waited = 0;
-  for (int i = 0; i < iters; i++) {
-    radio.write(&buttonState, sizeof(buttonState));
+  for (int i = 0; i < USIters; i++) {
+    radio.write(&ping, sizeof(ping));
     digitalWrite(trigPinR, HIGH);
     delayMicroseconds(10);
     digitalWrite(trigPinR, LOW);
     temp = pulseIn(echoPinR, HIGH);
-    if (temp > 30000 && waited < iters * 5) {
+    if (temp > 30000 && waited < USIters * 5) {
       i--;
       waited++;
     }
     else if (temp > 30000) {
-      durationR = iters * 40000;
+      durationR = USIters * 40000;
       break;
     }
     else {
@@ -143,34 +194,130 @@ void loop() {
     }
     delay(57);
   }
-  durationR /= iters;
+  durationR /= USIters;
   distanceR = msToIn(durationR);
   Serial.print(durationR);
   Serial.print(" R ");
   Serial.println(distanceR);
 
+  ping = false;
+  radio.write(&ping, sizeof(ping));
+  radio.openReadingPipe(1, pipe);
+  radio.startListening();
+
   if (durationL > 30000 && durationR > 30000) {
-    write1(0);
-    write2(0);
     Serial.println("Out of range!");
-  }
-  else if (durationL > 30000) {
-    write1(1);
-    write2(-1);
-    Serial.println("LS is lost!");
+    return false;
   }
   else if (durationR > 30000) {
-    write1(-1);
-    write2(1);
+    writeL(-1);
+    writeR(1);
     Serial.println("RS is lost!");
   }
+  else if (durationL > 30000) {
+    writeL(1);
+    writeR(-1);
+    Serial.println("LS is lost!");
+  }
   else {
-    //write1((double) (distanceL - 50) / 50.0 + (double) (distanceL - distanceR) / 40.0);
-    //write2((double) (distanceR - 50) / 50.0 + (double) (distanceR - distanceL) / 40.0);
-    write1(distanceR * 2 - distanceL);
-    write2(distanceL * 2 - distanceR);
+    writeL((double) (distanceL - 50) / 50.0 + (double) (distanceL - distanceR) / 40.0);
+    writeR((double) (distanceR - 50) / 50.0 + (double) (distanceR - distanceL) / 40.0);
+    //writeL(distanceL * 2 - distanceR);
+    //writeR(distanceR * 2 - distanceL);
     Serial.println("Good!");
   }
   delay(100);
-  Serial.println("\n");
+  return true;
+}
+
+void navGPS() {
+  Serial.println("US Failed, Using GPS");
+  NeoGPS::Location_t target ((long)x * 10000000L, (long)y * 10000000L);
+  distance = (float) fix.location.DistanceMiles(target) / 5280.0f;
+  if (bno08x.getSensorEvent(&sensorValue)) {
+    switch (sensorValue.sensorId) {
+      case SH2_ARVR_STABILIZED_RV:
+        quaternionToEulerRV(&sensorValue.un.arvrStabilizedRV, &ypr, true);
+      case SH2_GYRO_INTEGRATED_RV:
+        // faster (more noise?)
+        quaternionToEulerGI(&sensorValue.un.gyroIntegratedRV, &ypr, true);
+        break;
+    }
+  }
+  Serial.println(ypr.yaw);
+  myAngle = ypr.yaw;
+  angle = myAngle - fix.location.BearingToDegrees(target); //!! Check clockwise vs counter
+  if (distance < 10) { //Within 10 ft, just rotate
+    writeL(angle * 0.05); //! Calibrate turn speed
+    writeR(-angle * 0.05);
+  }
+  else if (distance < 50) { //Within 50 ft, move to it
+    writeL(angle * 0.03 + .5); //! Calibrate
+    writeR(-angle * 0.03 + .5);
+  }
+  else { //Out of 50 ft, consider itself lost
+    writeL(0);
+    writeR(0);
+  }
+}
+
+void loop() {
+  if (radio.available()) {
+    radio.read(&mode, sizeof(mode)); //Receive RF Message
+    delay(10);
+    while (c != '(') {
+      radio.read(&c, sizeof(c));
+      delay(10);
+    }
+    delay(10);
+    radio.read(&x, sizeof(x));
+    delay(10);
+    while (c != ',') {
+      radio.read(&c, sizeof(c));
+      delay(10);
+    }
+    delay(10);
+    radio.read(&y, sizeof(y));
+    while (c != ')') {
+      radio.read(&c, sizeof(c));
+      delay(10);
+    }
+    Serial.print(mode);
+    Serial.print("\t");
+    Serial.print(x);
+    Serial.print(", ");
+    Serial.println(y);
+
+    if (mode == 'J') { //Joystick Mode
+      navManual();
+    }
+    else if (mode == 'F') {
+      if (!navUS()) { //Try Ultrasonic
+        navGPS(); //If no US reception, use GPS
+      }
+    }
+    else {
+      writeL(0);
+      writeR(0);
+    }
+  }
+  while (gps.available(gpsPort)) { //UPDATE GPS !!!
+    gps_fix fix = gps.read();
+  }
+  if (bno08x.wasReset()) {
+    Serial.print("IMU was reset ");
+    setReports(reportType, reportIntervalUs);
+  }
+  if (bno08x.getSensorEvent(&sensorValue)) {
+    // in this demo only one report type will be received depending on FAST_MODE define (above)
+    switch (sensorValue.sensorId) {
+      case SH2_ARVR_STABILIZED_RV:
+        quaternionToEulerRV(&sensorValue.un.arvrStabilizedRV, &ypr, true);
+      case SH2_GYRO_INTEGRATED_RV:
+        // faster (more noise?)
+        quaternionToEulerGI(&sensorValue.un.gyroIntegratedRV, &ypr, true);
+        break;
+    }
+  }
+  delay(10);
 }
--- Golf_Transmit/Golf_Transmit.ino
+++ Golf_Transmit/Golf_Transmit.ino
@@ -1,39 +1,126 @@
 #include <SPI.h>
 #include <nRF24L01.h>
 #include <RF24.h>
-RF24 radio(9, 10); // CE, CSN
-const byte address[6] = "00001";
-boolean buttonState = 0;
+
+RF24 radio(8, 10); // CE, CSN
+
+
+#include <NMEAGPS.h>
+#include <GPSport.h>
+
+NMEAGPS gps;
+
+gps_fix fix;
+
+
 const int trigPin = 4;
 const int echoPin = 5;
-long duration;
-long distance;
-bool wasHigh = false;
+const int RPin = A5;
+const int GPin = A6;
+const int BPin = A7;
+const int LEDPin = 9;
+const int buttonPin = 2;
+const int xPin = A0;
+const int yPin = A1;
+const uint64_t pipe = 0xE6E6E6E6E6E6;
+const byte address[6] = "00001";
+
+long startT;
+int mode = 0;
+float x, y;
+char modeC;
+bool wasPressed = false;
+bool ping;
 
 void setup() {
-  pinMode(trigPin, OUTPUT);
-  pinMode(echoPin, INPUT);
   Serial.begin(9600);
-  //Serial.println("Begin");
-  radio.begin();
-  radio.openReadingPipe(0, address);   //Setting the address at which we will receive the data
-  radio.setPALevel(RF24_PA_MAX);       //You can set this as minimum or maximum depending on the distance between the transmitter and receiver.
-  radio.startListening();              //This sets the module as receiver
+  pinMode(buttonPin, INPUT_PULLUP);
+  radio.begin(); // Start the NRF24L01
+  radio.openWritingPipe(pipe);
+  radio.stopListening();
+  displayMode();
 }
 
-long msToIn(long ms) {
-  return ms / 74/* / 2*/;
+void displayMode() { //Show mode on RGB LED
+//Colors: 1 = Follow = Blue       2 = Joystick = Green          3 = Manual = Red
+  if (mode == 1) {
+    digitalWrite(RPin, LOW);
+    digitalWrite(GPin, LOW);
+    digitalWrite(BPin, HIGH);
+  }
+  if (mode == 2) {
+    digitalWrite(RPin, LOW);
+    digitalWrite(GPin, HIGH);
+    digitalWrite(BPin, LOW);
+  }
+  if (mode == 0) {
+    digitalWrite(RPin, HIGH);
+    digitalWrite(GPin, LOW);
+    digitalWrite(BPin, LOW);
+  }
+}
+
+void writeChar(char c) {
+  radio.write(&c, sizeof(c));
 }
 
 void loop() {
-  if (radio.available()) {
-    Serial.println("Data recieved, beginning");
-    radio.read(&buttonState, sizeof(buttonState));
-    //Serial.println(buttonState);
-    //if (buttonState == HIGH) {
-    digitalWrite(trigPin, HIGH);
-    delayMicroseconds(10);
-    digitalWrite(trigPin, LOW);
+  if (digitalRead(buttonPin) == LOW && !wasPressed) {
+    mode = (mode + 1) % 3;
+    displayMode();
+    wasPressed = true;
+    Serial.println(mode);
   }
-  delay(20);
+  if (digitalRead(buttonPin) == HIGH) {
+    wasPressed = false;
+  }
+  if (mode == 0) { //Stopped
+    x = 0;
+    y = 0;
+    modeC = 'S';
+  }
+  else if (mode == 1) { //Follow
+    //x = fix.latitude();
+    x = 20.0;
+    //y = fix.longitude();
+    y = 30.0;
+    modeC = 'F';
+  }
+  else if (mode == 2) { //Joystick
+    x = (float) analogRead(xPin);
+    y = 1023.0f - (float) analogRead(yPin);
+    modeC = 'J';
+  }
+  Serial.println(modeC);
+  writeChar(modeC);
+  writeChar('(');
+  radio.write(&x, sizeof(x));
+  writeChar(',');
+  radio.write(&y, sizeof(y));
+  writeChar(')');
+  if (mode == 1) {
+    ping = true;
+    radio.openReadingPipe(1, pipe);
+    radio.startListening();
+    startT = millis();
+    while (!radio.available() && millis() < startT + 2000) {
+      delay(20);
+    }
+    if (millis() < startT + 2000) {
+      while (ping == true) {
+        if (radio.available()) {
+          radio.read(&ping, sizeof(ping));
+          digitalWrite(trigPin, HIGH);
+          delayMicroseconds(10);
+          digitalWrite(trigPin, LOW);
+        }
+        delay(20);
+      }
+    }
+    radio.openWritingPipe(pipe);
+    radio.stopListening();
+  }
+  while (gps.available(gpsPort)) { //UPDATE GPS (and figure out port) !!!
+    gps_fix fix = gps.read();
+  }
 }
